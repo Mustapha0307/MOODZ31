47bd46874706f56ad9334fc2c51a25f5ec000a9e
Mustapha0307 <164336618+Mustapha0307@users.noreply.github.com>
2025-09-19 19:45:52 +0100

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -3,35 +3,39 @@ import { prisma } from "@/utils/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 
-
-
 const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  ...authConfig,
-
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        // أول مرة فقط
         token.role = user.role;
-      } else if (token.sub && !token.role) {
-        // الطلبات التالية: نجيب الدور من قاعدة البيانات
-        const dbUser = await prisma.user.findUnique({
-          where: { id: token.sub },
-          select: { role: true },
-        });
-        if (dbUser) token.role = dbUser.role;
       }
       return token;
     },
-
     async session({ session, token }) {
       if (session.user && token.sub) {
         session.user.id = token.sub;
-        session.user.role = token.role as string; // ← أضفنا الدور للـ session
+ 
+        const user = await prisma.user.findUnique({ where: { id: token.sub } });
+        if (user) {
+          session.user.role = user.role;
+          session.user.isTwoStepEnabled = user.isTwoStepEnabled;
+        }
       }
       return session;
     },
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
+
+      const userFromdb = await prisma.user.findUnique({
+        where: { id: user.id },
+      });
+      return true;
+    },
   },
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+
 });
-export {handlers, auth, signIn, signOut }
+
+export { handlers, auth, signIn, signOut };
