f4d4f727ff7c738539772fde24ce63e02dab499e
Mustapha0307 <164336618+Mustapha0307@users.noreply.github.com>
2025-09-19 16:08:49 +0100

    Update LoginForm.tsx

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -39,7 +39,7 @@ export default function LoginForm() {
           setServerSuccess(result.message || "");
 
           await update()
-          router.push("/profile/user")
+          router.replace("/profile/user")
           
         } else {
         setClientError(result.message || "Invalid credentials");
